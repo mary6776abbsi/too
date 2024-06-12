cda8921743d7cce4a9fefbdb9f9acdc540ecf172
maryam abbasi <169179074+mary6776abbsi@users.noreply.github.com>
2024-06-12 20:04:25 +0330

    too

---
+++ too.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number;
+    char character;
+
+    // دریافت عدد و کاراکتر از کاربر
+    cout << "Enter a number: ";
+    cin >> number;
+    cout << "Enter a character: ";
+    cin >> character;
+
+    // چاپ کاراکتر به تعداد عدد وارد شده
+    for (int i = 0; i < number; ++i) {
+        cout << character;
+    }
+
+    return 0;
+}
